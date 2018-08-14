bb9d64d3f8a2447b49b9edb555d4ab3687e9d3ce
Antoaneta Katarova <antoanetad78@gmail.com>
2018-08-14 12:55:18 +0300

    Added authenticate to GET /todos, GET/todos/:id, DELETE, PATCH making them private

--- server/server.js
+++ server/server.js
@@ -17,9 +17,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
@@ -29,16 +30,18 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e)=> {
     res(400).send(e);
   });
 });
 
-//GET /todos/12548
-app.get('/todos/:id', (req,res) => {
+//GET /todos/id
+app.get('/todos/:id', authenticate, (req,res) => {
   var id = req.params.id;
   //Validate id usin isValid
     //404 - send back empty body - send();
@@ -51,7 +54,10 @@ app.get('/todos/:id', (req,res) => {
          //if todo send it back
          //if no todo - send back 404 with empty body
      //error - 404 and empty body
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id:id,
+    _creator: req.user._id
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send('Sorry, we cannot find that!');
     }
@@ -61,14 +67,17 @@ app.get('/todos/:id', (req,res) => {
 
 //findByIdAndRemove
 
-app.delete('/todos/:id', (req,res) => {
+app.delete('/todos/:id', authenticate, (req,res) => {
 
   var id = req.params.id;
 
   if(!ObjectID.isValid(id)) {
     return res.status(400).send('<h3>Remove failed: Invalid Object ID</h3>')
   }
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findOneAndRemove({
+    _id:id,
+    _creator: req.user._id
+  }).then((todo) => {
     if(!todo) {
       return res.status(404).send('<h4>Todo not found!</h4>')
     }
@@ -76,7 +85,7 @@ app.delete('/todos/:id', (req,res) => {
   }).catch((e)=> res.status(400).send('<h4>An error occured</h4>'));
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']); // _.pick takes an object (request.body in this case - and sends to the var body only the parameters inthe array. The user will see only these params)
 
@@ -91,7 +100,12 @@ app.patch('/todos/:id', (req, res) => {
       body.completedAt = null
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    Todo.findOneAndUpdate({
+      _id:id,
+      _creator: req.user._id
+    },
+    {$set: body},
+    {new: true}).then((todo) => {
       if(!todo) {
         return res.status(404).send('<h3>Todo not found</h3>');
       }
